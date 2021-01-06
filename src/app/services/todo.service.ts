162d14ab48571889b6150078e3a97c9ba64b6d31
JonasGertz <Jonas.Gertz@hotmail.com>
2021-01-06 15:44:03 +0100

    idtoken cognito, still cors problem

--- src/app/services/todo.service.ts
+++ src/app/services/todo.service.ts
@@ -1,36 +1,59 @@
-import { Injectable } from '@angular/core';
+import { Injectable, OnInit } from '@angular/core';
 import {HttpClient, HttpHeaders} from '@angular/common/http'
 import { Observable } from 'rxjs';
 import { Todo } from '../models/Todo';
-
-const httpOptions = {
-  headers: new HttpHeaders({
-    "x-api-key": "key here guis"
-  })
-}
-
+import { Auth } from 'aws-amplify';
 
 @Injectable({
   providedIn: 'root'
 })
-export class TodoService {
-  URL: string = "https://ajy3tsqb2d.execute-api.eu-west-1.amazonaws.com/dev/todos"
 
+export class TodoService implements OnInit{
+  URL: string = "https://ajy3tsqb2d.execute-api.eu-west-1.amazonaws.com/dev/todos"
   constructor(private http: HttpClient) { }
 
+  token: string;
+  httpOptions: object;
+
+
+  ngOnInit(): void {
+
+  }
+
+  checkToken(): void {
+    Auth.currentSession().then(res=>{
+      let idToken = res.getIdToken()
+      this.token = idToken.getJwtToken()
+      console.log('idToken:', this.token );
+      this.httpOptions = {
+        headers: new HttpHeaders({
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Headers": "*",
+          Authorization: this.token
+        })
+      }
+    })
+  }
+
   getTodos():Observable<Todo[]> {
-    return this.http.get <Todo[]> (this.URL, httpOptions) 
+    this.checkToken()
+    console.log(this.httpOptions);
+    
+    return this.http.get <Todo[]> (this.URL, this.httpOptions) 
   }
 
   addTodo(todo: Todo):Observable<Todo> {
-    return this.http.post <any> (this.URL, todo)
+    this.checkToken()
+    return this.http.post <any> (this.URL, todo, this.httpOptions)
   }
 
   deleteTodo(todo: Todo):Observable<any> {
-    return this.http.delete <any> (`${this.URL}/object/${todo.id}`)
+    this.checkToken()
+    return this.http.delete <any> (`${this.URL}/object/${todo.id}`, this.httpOptions)
   }
 
   toggleCompleted(todo: Todo):Observable<Todo> {
-    return this.http.put <Todo> (`${this.URL}`, todo)
+    this.checkToken()
+    return this.http.put <Todo> (`${this.URL}`, todo, this.httpOptions)
   }
 }
